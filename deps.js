eb6a969ede23ef95931738b1e766c23c7112771d
Mark Story <mark@mark-story.com>
2010-12-29 11:34:22 -0500

    Getting some of the library setup, adding what will be an executable.

--- deps.js
+++ deps.js
@@ -2,5 +2,5 @@ module.exports = {
 	vows: require('vows'),
 	sys: require('sys'),
 	assert: require('assert'),
-	net: require('net')
+	http: require('http')
 };
--- test/1-server-test.js
+++ test/1-server-test.js
@@ -1,11 +1,11 @@
 var deps = require('../deps.js');
-var jerk = require('../jerk.js');
+var jerk = require('../lib/jerkbot.js');
 
 var vows = deps.vows;
 var assert = deps.assert;
 
 vows.describe('Server creation').addBatch({
-	'Server should exist and be net.Server': function () {
-		assert.ok(jerk instanceof deps.net.Server)
+	'Server should exist and be http.Server': function () {
+		assert.ok(jerk.server instanceof deps.http.Server)
 	}
 }).export(module)
