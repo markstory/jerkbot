50817a4216011a153ec4cb11c709239c9a386a35
Mark Story <mark@mark-story.com>
2010-12-29 22:34:42 -0500

    Moving test cases.  Just use `vows` to run the test cases.

---
+++ test/attitude/attitude-half-test.js
@@ -0,0 +1,49 @@
+var vows = require('vows'),
+	assert = require('assert'),
+	half = require('../../lib/attitude/half'),
+	http = require('http');
+
+var mockResponse = {
+	write: function (content) {
+		this.content = content
+	},
+
+	writeHead: function (code, headers) {
+		this.code = code;
+		this.headers = headers;
+	}
+};
+
+vows.describe('Half length attitude').addBatch({
+	'working with canned responses': {
+		topic: function () {
+			return Object.create(half);
+		},
+
+		'Takes a canned response value': function (topic) {
+			assert.ok(topic.setResponse('This is a string value'));
+		},
+	
+		'Returns half the response on run()': function (topic) {
+			topic.setResponse('This this ');
+
+			var httpResponse = Object.create(mockResponse);
+			assert.ok(topic.run(httpResponse));
+			assert.equal(httpResponse.content, 'This ');
+		},
+
+		'Sets headers correctly': function (topic) {
+			var headers = {'Content-Type': 'text/html'};
+
+			topic.setResponse('test');
+			topic.setHead(300, headers);
+	
+			var httpResponse = Object.create(mockResponse);
+
+			assert.ok(topic.run(httpResponse));
+			assert.equal(300, httpResponse.code);
+			assert.deepEqual(headers, httpResponse.headers);
+		}
+	}
+}).export(module)
+
--- test/attitude/nice-attitude-test.js
+++ test/attitude/nice-attitude-test.js
@@ -1,6 +1,6 @@
 var vows = require('vows'),
 	assert = require('assert'),
-	half = require('../lib/attitude/half'),
+	nice = require('../../lib/attitude/nice'),
 	http = require('http');
 
 var mockResponse = {
@@ -14,35 +14,26 @@ var mockResponse = {
 	}
 };
 
-vows.describe('Half length attitude').addBatch({
+vows.describe('Nice attitude').addBatch({
 	'working with canned responses': {
 		topic: function () {
-			return Object.create(half);
+			return Object.create(nice);
 		},
 
 		'Takes a canned response value': function (topic) {
 			assert.ok(topic.setResponse('This is a string value'));
 		},
 	
-		'Returns half the response on run()': function (topic) {
-			topic.setResponse('This this ');
-
-			var httpResponse = Object.create(mockResponse);
-			assert.ok(topic.run(httpResponse));
-			assert.equal(httpResponse.content, 'This ');
-		},
-
-		'Sets headers correctly': function (topic) {
+		'does not muck around with them': function (topic) {
 			var headers = {'Content-Type': 'text/html'};
 
-			topic.setResponse('test');
-			topic.setHead(300, headers);
-	
+			topic.setResponse('<tag>stuff</tag>');
+			topic.setHead(200, headers);
+
 			var httpResponse = Object.create(mockResponse);
 
 			assert.ok(topic.run(httpResponse));
-			assert.equal(300, httpResponse.code);
-			assert.deepEqual(headers, httpResponse.headers);
+			assert.equal(httpResponse.content, '<tag>stuff</tag>');
 		}
 	}
 }).export(module)
