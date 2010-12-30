b4cf984122f2cc21419afd88fef4a9b8d2214c79
Mark Story <mark@mark-story.com>
2010-12-29 22:20:55 -0500

    Adding tests for config file loading.

--- test/1-server-test.js
+++ test/1-server-test.js
@@ -1,14 +1,36 @@
 var vows = require('vows'),
 	assert = require('assert'),
-	jerk = require('../lib/jerkbot.js'),
-	http = require('http');
+	path = require('path'),
+	http = require('http'),
 
-vows.describe('jerkbot server').addBatch({
-	'should have a createServer function': function () {
-		assert.isFunction(jerk.createServer)
+	jerk = require('../lib/jerkbot.js');
+
+
+vows.describe('jerkbot').addBatch({
+	'server': {
+		'should have a createServer function': function () {
+			assert.isFunction(jerk.createServer)
+		},
+
+		'createServer should make an http.Server': function () {
+			assert.ok(jerk.createServer({}) instanceof http.Server);
+		}
 	},
+	'loadConfig with empty config file': {
+		topic: function () {
+			return path.join(__dirname, 'config', 'empty_config.js');
+		},
 
-	'createServer should make an http.Server': function () {
-		assert.ok(jerk.createServer({}) instanceof http.Server);
+		'should set port to 8080 when empty': function (topic ) {
+			var config = jerk.loadConfig(topic);
+
+			assert.equal(8080, config.port);
+		},
+
+		'should leave host undefined': function (topic) {
+			var config = jerk.loadConfig(topic);
+
+			assert.equal(undefined, config.host);
+		}
 	}
 }).export(module)
