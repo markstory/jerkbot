ab86aa8fe8c56d1dcd8df155976b74281f5b9522
Mark Story <mark@mark-story.com>
2011-01-02 11:56:20 -0500

    Renaming refuse -> drop.

--- test/attitude/drop-test.js
+++ test/attitude/drop-test.js
@@ -1,20 +1,20 @@
 var vows = require('vows'),
 	assert = require('assert'),
-	refuse = require('../../lib/attitude/refuse'),
+	drop = require('../../lib/attitude/drop'),
 	mocks = require('../mocks.mock');
 
-vows.describe('Refuse attitude').addBatch({
+vows.describe('Drop attitude').addBatch({
 	'responding': {
 		topic: function () {
-			var refuser = new(refuse);
-			refuser.setResponse('This is a response');
+			var dropper = new(drop);
+			dropper.setResponse('This is a response');
 
 			var httpResponse = Object.create(mocks.httpResponse);
 			httpResponse.connection = {
 				destroy: this.callback
 			};
 
-			refuser.run(httpResponse);
+			dropper.run(httpResponse);
 			this.response = httpResponse;
 		},
 
